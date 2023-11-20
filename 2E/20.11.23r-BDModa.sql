c9a0b40a3388286c94d74881e6c414dc99e4798f
Zuza Jerzyńska <114587290+Zusza@users.noreply.github.com>
2023-11-20 08:37:10 +0100

    Update and rename kwerenda1.sql to 20.11.23r-BDModa.sql

---
+++ 2E/20.11.23r-BDModa.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS SalonMody;
+
+CREATE DATABASE SalonMody;
+
+USE SalonMody;
+DROP DATABASE IF EXISTS Kreacja;
+DROP DATABASE IF EXISTS Material;
+DROP DATABASE IF EXISTS Stroj;
+CREATE TABLE Stroj (
+    idStroj INT PRIMARY KEY AUTO_INCREMENT,
+    nazwaStroj VARCHAR(255),
+    dataUtworzenia DATE,
+    dataWaznosci DATE,
+    firma VARCHAR(255),
+    wlasciciel VARCHAR(255)
+);
+
+CREATE TABLE Material (
+    idMaterial INT PRIMARY KEY AUTO_INCREMENT,
+    nazwaMaterial VARCHAR(255),
+    kategoria VARCHAR(100),
+    kolor VARCHAR(100),
+    rozmiar VARCHAR(1)
+);
+
+CREATE TABLE Kreacja (
+    idKreacja INT PRIMARY KEY AUTO_INCREMENT,
+    idStroj INT,
+    idMaterial INT,
+    FOREIGN KEY (idStroj) REFERENCES Stroj(idStroj),
+    FOREIGN KEY (idMaterial) REFERENCES Material(idMaterial),
+    dataUtworzenia DATE
+);
+
+INSERT INTO Stroj VALUES(NULL,"Wieczorowy", "2023-11-20", "2023-12-20", "T19", "Bartosz");
+INSERT INTO Stroj VALUES(NULL,"Skatowy", "2023-11-19", "2023-12-24", "Skatepark", "Wojciech");
+
